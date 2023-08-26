d078e84ba3b35c169096c47d294ca4a9765682c9
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-08-26 22:40:00 +0300

    Добавляет новые блоки

---
+++ source/js/main.js
@@ -0,0 +1,70 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+import {Form} from './modules/form-validate/form';
+
+// ---------------------------------
+
+const toggleSubmenu = () => {
+  const modalMenu = document.querySelector('.modal-menu');
+
+  if (!modalMenu) {
+    return;
+  }
+
+  const subButtons = modalMenu.querySelectorAll('.modal-menu__sub-button');
+
+  subButtons.forEach((subButton) => {
+    subButton.addEventListener('click', () => {
+      const subMenu = subButton.nextElementSibling;
+      subMenu.classList.toggle('modal-menu__sub-menu--closed');
+    });
+  });
+};
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+    const form = new Form();
+    window.form = form;
+    form.init();
+    toggleSubmenu();
+  });
+});
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используется matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
