54e791bcd7f64a4ebdf0715da9f1e0379b80abd6
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-08-27 21:21:22 +0300

    Добавляет hero-блок

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,35 @@
 // Swiper 7.4.1
 import './vendor/swiper';
-import './vendor/focus-visible-polyfill';
+// import './vendor/focus-visible-polyfill';
 
+const initHeroSwiper = () => {
+  // const swiperElement = document.querySelectorAll('.hero__swiper');
+  // const paginationElement = document.querySelector('.hero__pagination');
+
+  // eslint-disable-next-line
+  const heroSwiper = new Swiper('.hero__swiper', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.hero__pagination',
+      clickable: true,
+    },
+    speed: 0,
+    autoplay: {
+      delay: 3000,
+    },
+  });
+
+  heroSwiper[0].on('slideChange', (s) => {
+    // eslint-disable-next-line
+    heroSwiper[1].slideTo(s.activeIndex)
+  });
+
+  heroSwiper[1].on('slideChange', (s) => {
+    // eslint-disable-next-line
+    heroSwiper[0].slideTo(s.activeIndex)
+  });
+};
+
+
+initHeroSwiper();
