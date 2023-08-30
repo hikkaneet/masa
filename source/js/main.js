6895b86e172af077b91ecf5b7b63e54e72ea6f93
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-08-30 08:47:42 +0300

    Подключает custom-select

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {CustomSelect} from './modules/select/custom-select';
 import {Form} from './modules/form-validate/form';
 
 // ---------------------------------
@@ -37,6 +38,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+    const select = new CustomSelect();
+    select.init();
     const form = new Form();
     window.form = form;
     form.init();
